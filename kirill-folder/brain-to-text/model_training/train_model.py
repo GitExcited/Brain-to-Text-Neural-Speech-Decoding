9d623e1733c71a85c5d75191d8e07a88f86f6fd7
Kirill Vishnyakov <kir.vishn@gmail.com>
2025-10-05 21:27:42 -0400

    typo

--- kirill-folder/brain-to-text/model_training/train_model.py
+++ kirill-folder/brain-to-text/model_training/train_model.py
@@ -2,6 +2,6 @@ from omegaconf import OmegaConf
 from rnn_trainer import BrainToTextDecoder_Trainer
 
 
-args = OmegaConf.load('model_training/kirill-folder/brain-to-text/rnn_args.yaml')
+args = OmegaConf.load('kirill-folder/brain-to-text/model_training/rnn_args.yaml')
 trainer = BrainToTextDecoder_Trainer(args)
 metrics = trainer.train()
